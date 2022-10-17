c5b8190cc8d1acc72310b4fdb44be3a36b69b558
LJ <lavishjain36@gmail.com>
2022-10-17 12:27:36 +0530

    Add files via upload
    Inclded object blog

---
+++ index.js
@@ -0,0 +1,81 @@
+// // let or const=>ES-6  var=>Es-1
+// //  Reference Type of data 
+// // Array=>
+
+// // let array=[10,20,30,40,50];
+// // console.log(array[0]);//10
+
+// // console.log(array[10]);//undefined
+
+
+// // let array=[20,40,50,60,10,30]
+// // //sort =>reverse 
+// // console.log(array.sort().reverse())
+
+// // Splice(index,no of the element that we remove)
+// // let data=["Apple","Banana","Grapes","Mangoes","Oranges"]
+// // console.log(data.splice(1,2)); //banana ,grapes
+// // console.log(data)//
+
+// // 1.push
+// // 2.pop 
+// // 3.Splice
+// // 4.slice
+// // 5.join 
+// // 6.split('')
+
+
+
+// let data=10;
+// let newdata;
+// //copy the data value 10 to newdata 
+// newdata=data;
+// console.log(newdata);
+// console.log(data);
+
+
+
+// //Copy by Reference
+// let array1=[10,20,30,40]
+// let array2=[12,34,64,89]
+// console.log(array1);
+// console.log(array2);
+// array2=array1;
+// array2.push(100)
+// console.log(array1);
+// console.log(array2);
+
+// // modifying the elements of one array is also affecting another array 
+// // spread Operator
+
+
+//Object Literal
+// let obj={
+//     brand:"Maruti",
+//     color:"white",
+//     length:"100",
+//     height:"100",
+//     seats:7
+
+// }
+
+// console.log(obj.brand)//dot operator
+// console.log(obj['seats'])//Bracket Notation
+
+
+
+// Different ways to create an object 
+// 1.new keyword
+
+Write a blog on the Difference between document & window objects
+code:
+Object Literal
+// let obj={
+//     brand:"Maruti",
+//     color:"white",
+//     length:"100",
+//     height:"100",
+//     seats:7
+Output:
+Maruti
+7
